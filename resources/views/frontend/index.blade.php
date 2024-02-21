dd82b453072da9d304640567151c7bdd4caeaa61
syaafiudinm <altafpasallo12@gmail.com>
2024-02-21 22:16:07 +0800

    fixed jumbotron

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,8 +8,8 @@
         <div class="container">
 
             <div class="jumbotron">
-                <h1 class="display-4">Hello, User!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 class="display-4">Hello, Admin!</h1>
+                <p class="lead">This is a simple admin page for controlling the products, categories, and gallery</p>
                 <hr class="my-4">
                 <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
                 <a class="btn btn-primary btn-lg" href="{{url('products')}}" role="button">see Product!</a>
